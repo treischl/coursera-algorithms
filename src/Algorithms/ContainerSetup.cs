fb0a55f0c9cc2cc037555088817c25596e8c7eba
Tristan Reischl <treischl@gmail.com>
2020-03-24 11:01:57 -0700

    Start course again
    Using `dotnet` this time around.

---
+++ src/Algorithms/ContainerSetup.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace Algorithms
+{
+    public static class ContainerSetup
+    {
+        public static IContainer Initialize()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(typeof(ContainerSetup).Assembly)
+                .AsImplementedInterfaces();
+
+            return builder.Build();
+        }
+    }
+}
