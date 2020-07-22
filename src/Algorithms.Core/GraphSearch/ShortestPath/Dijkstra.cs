a66a14dbf035b82bd5ef5671e3a603acf74c69b0
Tristan Reischl <treischl@gmail.com>
2020-07-22 12:31:48 -0700

    Add Dijkstra shortest path unit test

---
+++ src/Algorithms.Core/GraphSearch/ShortestPath/Dijkstra.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Core.GraphSearch.ShortestPath
+{
+    public class Dijkstra : IDijkstra
+    {
+        public IDictionary<DijkstraVertex, int> CalculateShortestPaths(DijkstraAdjacencyList graph, int sourceLabel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Algorithms.Core/GraphSearch/ShortestPath/DijkstraAdjacencyList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Core.GraphSearch.ShortestPath
+{
+    public class DijkstraAdjacencyList
+    {
+        public List<DijkstraVertex> Vertices { get; } = new List<DijkstraVertex>();
+        public List<DijkstraEdge> Edges { get; } = new List<DijkstraEdge>();
+    }
+}
