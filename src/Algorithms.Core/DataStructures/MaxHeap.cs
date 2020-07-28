437a930437df21fa66cc2e587d9151c0cf2e082e
Tristan Reischl <treischl@gmail.com>
2020-07-28 00:52:08 -0700

    Add min and max Heap implementations

---
+++ src/Algorithms.Core/DataStructures/MaxHeap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Algorithms.Core.DataStructures
+{
+    public class MaxHeap<T> : Heap<T>
+        where T : struct, IComparable<T>
+    {
+        protected override bool CompareItems(T item1, T item2)
+        {
+            return item1.CompareTo(item2) < 0;
+        }
+    }
+}
