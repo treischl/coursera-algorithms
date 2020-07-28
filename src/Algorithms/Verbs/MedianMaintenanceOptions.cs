cfc119c73486a11fd8d67883d36dc7a8475334a6
Tristan Reischl <treischl@gmail.com>
2020-07-28 01:04:13 -0700

    Add Median Maintenance command to console app
    - Also fixed a grammatical error in the Dijkstra command's help text

---
+++ src/Algorithms/Verbs/MedianMaintenanceOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace Algorithms.Verbs
+{
+    [Verb("median-maintenance", HelpText = "Calculate the sum of medians from a list of integers")]
+    public class MedianMaintenanceOptions
+    {
+        [Option('f', "file", HelpText = "Path to a text file with a list of integers.", Required = true)]
+        public string File { get; set; } = string.Empty;
+    }
+}
