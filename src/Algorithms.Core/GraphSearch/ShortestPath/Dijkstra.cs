09c6c7cb828f00e4e6cf9cac8a8e64709ee85b44
Tristan Reischl <treischl@gmail.com>
2020-07-27 21:02:53 -0700

    Refactor and optimize Dijkstra.CalculateShortestPaths
    - Removed several extraneous loops by relying less on Linq

--- src/Algorithms.Core/GraphSearch/ShortestPath/Dijkstra.cs
+++ src/Algorithms.Core/GraphSearch/ShortestPath/Dijkstra.cs
@@ -10,49 +10,73 @@ namespace Algorithms.Core.GraphSearch.ShortestPath
             int sourceLabel
         )
         {
-            var sourceVertex = graph.Vertices.First(x => x.Label == sourceLabel);
-            var processedVertices = new List<DijkstraVertex> { sourceVertex };
-            var shortestPaths = graph.Vertices.ToDictionary(
-               vertex => vertex,
-               vertex => vertex.Label == sourceLabel ? 0 : int.MaxValue
-            );
+            var (processed, frontier, scores) = InitializeCollections(graph, sourceLabel);
 
-            DijkstraEdge[] frontierEdges = graph.Edges
-                .Where(edge =>
-                    processedVertices.Contains(edge.Vertices[0]) !=
-                    processedVertices.Contains(edge.Vertices[1])
-                )
-                .ToArray();
-            while (frontierEdges.Length > 0)
+            while (frontier.Count > 0)
             {
-                var lengths = frontierEdges.ToDictionary(
-                    edge => edge,
-                    edge =>
-                    {
-                        var processedVertex = edge.Vertices
-                            .First(vertex => processedVertices.Contains(vertex));
-                        var notProcessedVertex = edge.Vertices
-                            .First(vertex => !processedVertices.Contains(vertex));
-
-                        return shortestPaths[processedVertex] + edge.Length;
-                    }
-                );
-                var shortestPath = lengths.Values.Min();
-                var edgeWithShortestPath = lengths.First(kvp => kvp.Value == shortestPath).Key;
-                var notProcessedVertex = edgeWithShortestPath.Vertices
-                            .First(vertex => !processedVertices.Contains(vertex));
-                processedVertices.Add(notProcessedVertex);
-                shortestPaths[notProcessedVertex] = shortestPath;
-
-                frontierEdges = graph.Edges
-                    .Where(edge =>
-                        processedVertices.Contains(edge.Vertices[0]) !=
-                        processedVertices.Contains(edge.Vertices[1])
-                    )
-                    .ToArray();
+                var (minEdge, minScore) = FindMinimizingEdge(processed, frontier, scores);
+                var unprocessedVertex = minEdge.Vertices.First(vertex => !processed.Contains(vertex));
+                processed.Add(unprocessedVertex);
+                scores[unprocessedVertex] = minScore;
+                UpdateFrontierEdges(frontier, processed, unprocessedVertex);
             }
 
-            return shortestPaths;
+            return scores;
+        }
+
+        private (
+            List<DijkstraVertex> processedVertices,
+            List<DijkstraEdge> frontierEdges,
+            Dictionary<DijkstraVertex, int> shortestPaths
+        ) InitializeCollections(DijkstraAdjacencyList graph, int sourceLabel)
+        {
+            var sourceVertex = graph.Vertices.First(vertex => vertex.Label == sourceLabel);
+            var processedVertices = new List<DijkstraVertex>() { sourceVertex };
+            var frontierEdges = new List<DijkstraEdge>(sourceVertex.Edges);
+            var shortestPaths = graph.Vertices.ToDictionary(
+                vertex => vertex,
+                vertex => vertex == sourceVertex ? 0 : int.MaxValue);
+
+            return (processedVertices, frontierEdges, shortestPaths);
+        }
+
+        private (DijkstraEdge edge, int score) FindMinimizingEdge(
+            List<DijkstraVertex> processedVertices,
+            List<DijkstraEdge> frontierEdges,
+            Dictionary<DijkstraVertex, int> shortestPaths
+        )
+        {
+            DijkstraEdge minEdge = null;
+            var minScore = int.MaxValue;
+            foreach (var edge in frontierEdges)
+            {
+                var processedVertex = processedVertices.Contains(edge.Vertices[0])
+                    ? edge.Vertices[0] : edge.Vertices[1];
+                var edgeScore = shortestPaths[processedVertex] + edge.Length;
+                if (edgeScore < minScore)
+                {
+                    minEdge = edge;
+                    minScore = edgeScore;
+                }
+            }
+            return (minEdge, minScore);
+        }
+
+        private void UpdateFrontierEdges(
+            List<DijkstraEdge> frontierEdges,
+            List<DijkstraVertex> processedVertices,
+            DijkstraVertex justProcessedVertex)
+        {
+            frontierEdges.RemoveAll(BothVerticesAreProcessed);
+            var edgesToAdd = justProcessedVertex.Edges.Where(HasAnUnprocessedVertex);
+            frontierEdges.AddRange(edgesToAdd);
+
+            bool BothVerticesAreProcessed(DijkstraEdge edge) =>
+                processedVertices.Contains(edge.Vertices[0]) &&
+                processedVertices.Contains(edge.Vertices[1]);
+
+            bool HasAnUnprocessedVertex(DijkstraEdge edge) =>
+                !BothVerticesAreProcessed(edge);
         }
     }
 }
