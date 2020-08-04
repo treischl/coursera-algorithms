ce743b337e3f9b8e2756e22f1411222327fe2c25
Tristan Reischl <treischl@gmail.com>
2020-08-03 20:48:52 -0700

    Add Two Sum command to console app

---
+++ src/Algorithms/Verbs/TwoSumOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace Algorithms.Verbs
+{
+    [Verb("two-sum", HelpText = "Calculate the number of distinct target values which can be produced from a list of integers.")]
+    public class TwoSumOptions
+    {
+        [Option('f', "file", HelpText = "Path to a text file with a list of integers.", Required = true)]
+        public string File { get; set; } = string.Empty;
+
+        [Option('s', "start", HelpText = "Starting integer for the target range.")]
+        public long Start { get; set; } = -10000;
+
+        [Option('e', "end", HelpText = "Ending integer for the target range.")]
+        public long End { get; set; } = 10000;
+    }
+}
