d3f06f5fe8b97f4a6ee897a1ad115d93a0e9720b
Tristan Reischl <treischl@gmail.com>
2020-08-03 19:45:22 -0700

    Add Two Sum unit test

---
+++ src/Algorithms.Core/GraphSearch/ITwoSum.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Core.GraphSearch
+{
+    public interface ITwoSum
+    {
+        IEnumerable<long> GetDistinctSums(
+            IDictionary<long, long> numbers,
+            IEnumerable<long> targets
+        );
+    }
+}
---
+++ src/Algorithms.Core/GraphSearch/TwoSum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Core.GraphSearch
+{
+    public class TwoSum : ITwoSum
+    {
+        public IEnumerable<long> GetDistinctSums(IDictionary<long, long> numbers, IEnumerable<long> targets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
